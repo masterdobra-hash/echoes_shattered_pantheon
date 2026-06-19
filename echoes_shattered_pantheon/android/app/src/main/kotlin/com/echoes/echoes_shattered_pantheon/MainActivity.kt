e19e0fe42239e66abbc60fb3dc4a12bf19a3a4fe
masterdobra-hash <masterdobra@gmail.com>
2026-06-19 10:30:38 +0300

    v1
    CI CD from APK

--- echoes_shattered_pantheon/android/app/src/main/kotlin/com/echoes/echoes_shattered_pantheon/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.echoes.echoes_shattered_pantheon
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ echoes_shattered_pantheon/android/app/src/main/kotlin/com/echoes/shatteredpantheon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.echoes.shatteredpantheon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
